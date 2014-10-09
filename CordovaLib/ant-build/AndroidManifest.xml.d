4fd02ae19e2f07ed3c8492087840aa902b71d121
justin73 <jiameng_73@hotmail.com>
2014-10-08 23:12:49 -0400

    first commit of KindnessApp

---
+++ CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\Programming Practice\PhoneGap\kindnessApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+E:\Programming Practice\PhoneGap\kindnessApp\platforms\android\CordovaLib\AndroidManifest.xml \
