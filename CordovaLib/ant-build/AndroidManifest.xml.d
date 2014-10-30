bb1ea060430dfcfa4d014296f8faacd373534e01
Kwaku Zigah <kwaku.zigah@fordemobile.com>
2014-10-29 21:53:40 -0400

    upgraded project code

--- CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\Programming Practice\PhoneGap\kindnessApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-E:\Programming Practice\PhoneGap\kindnessApp\platforms\android\CordovaLib\AndroidManifest.xml \
