4d7058f2c939b6814842113db49d2f66d8614aae
Meng <ios_dev@ios-devs-MacBook-Pro-2.local>
2015-01-23 00:19:53 -0500

    fixed FAQ display bug

--- platforms/android/assets/www/js/kindness-app.js
+++ platforms/android/assets/www/js/kindness-app.js
@@ -152,15 +152,9 @@ function initAlarmManager(init) {
     }
 
     function selectAlarmSuccess(tx, results) {
-
-//        window.plugin.notification.local.registerPermission(function (granted) {
-//            alert(granted);
-//        });
-//        window.plugin.notification.local.add({ message: 'Great app!' });
-        alert('here');
         var eventsArray = [];
         // the number of rows returned by the select statement
-        console.log('select alarms success: ' + results.rows.length);
+//        console.log('select alarms success: ' + results.rows.length);
         $("#list-alarms").empty();
         for (var i =0; i < results.rows.length; i++) {
             var id = results.rows.item(i).id;
@@ -301,13 +295,13 @@ function playGong() {
 
 function initQuote() {
     var start = new Date(window.localStorage.getItem("startDate"));
-    alert(start);
+//    alert(start);
     var end = new Date();
-    alert(end);
+//    alert(end);
     var diff = new Date(end - start);
 
     var days = Math.floor(diff/1000/60/60/24);
-    alert(days);
+//    alert(days);
 //    $("#quote").html(jsonObject.Quote[days].Content);
 //    $("#author").html(jsonObject.Quote[days].Writer);
 }
